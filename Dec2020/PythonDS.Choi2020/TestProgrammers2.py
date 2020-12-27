b299dcae43ff28256b904fe7e4d1135a999bfb06
sunnyw <vnvnchlrh@gmail.com>
2020-12-28 02:29:06 +0900

    20/12/28 02:28

---
+++ Dec2020/PythonDS.Choi2020/TestProgrammers2.py
@@ -0,0 +1,29 @@
+def solution(arr):
+    answer = []
+    elm  = min(arr)
+
+    if len(arr) > 1:
+        for i in range(0, len(arr)-1):
+            if elm == arr[i]:
+                arr.pop(i)
+                answer = arr
+
+    else: answer = [-1]
+
+    return answer
+
+
+### Inputs
+
+arr1 = [4, 3, 2, 1]
+solution(arr1)
+
+arr2 = [10]
+solution(arr2)
+
+
+def plusfour(k):
+    print(k+4)
+
+m = 1
+plusfour(m)
