c5c21c56c51199f78ab131aa4fffe2bca281ac3c
sunnyw <vnvnchlrh@gmail.com>
2020-12-30 20:25:59 +0900

    20/12/30 20:25

---
+++ Codeup/basics#40-60.py
@@ -0,0 +1,30 @@
+#basics #40-60
+
+#40
+
+a = input()
+a = int(a)
+print(-a)
+
+
+#42
+
+a, b = map(int, input().split(" "))
+print(a//b)
+
+#47
+
+a = input()
+a = int(a)
+print(a*2)
+
+#50
+
+a, b = map(int, input().split(" "))
+if a==b:
+    print(1)
+else:
+    print(0)
+
+
+
