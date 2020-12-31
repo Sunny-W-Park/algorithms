1a6e552a504061ecf9dc47046beec9e003664f31
sunnyw <vnvnchlrh@gmail.com>
2020-12-31 10:23:41 +0900

    20/12/31 10:23

---
+++ Codeup/basics#21-39.py
@@ -0,0 +1,22 @@
+###basics#21-39
+
+
+#21.
+
+a = input()
+a = str(a)
+print(a)
+
+
+#22.
+
+a = input()
+a = str(a)
+print(a)
+
+
+#24. 
+
+a, b = map(int, input().split(" "))
+print(a + b)
+
--- Codeup/basics#40-60.py
+++ Codeup/basics#40-60.py
@@ -12,11 +12,34 @@ print(-a)
 a, b = map(int, input().split(" "))
 print(a//b)
 
+
+#43
+
+a, b = map(int, input().split(" "))
+print(a%b)
+
+
+#44
+
+a = input()
+print(a + 1)
+
+
 #47
 
 a = input()
 a = int(a)
 print(a*2)
+
+
+#49-52
+
+a, b = map(int, input().split(""))
+if a>b:
+    print(1)
+else:
+    print(0)
+
 
 #50
 
