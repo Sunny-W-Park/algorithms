567b8679b8fcc6935c8b3d859110ea144d165755
sunnyw <vnvnchlrh@gmail.com>
2021-01-19 00:54:59 +0900

    210119 00:54

--- Codeup/recursive.py
+++ Codeup/recursive.py
@@ -2,6 +2,14 @@
 
 n = int(input())
 
+def recursive_factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * recursive_factorial(n-1)
+
+print(recursive_factorial(n))
+
 
 #1904
 import sys
