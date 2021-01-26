3125dfd64dd92de447449a259d346a2c72d46d94
sunnyw <vnvnchlrh@gmail.com>
2021-01-26 14:08:42 +0900

    210126 14:08

--- PythonThisisCS.Na2020/Chapter7.BinarySearch.py
+++ PythonThisisCS.Na2020/Chapter7.BinarySearch.py
@@ -1,6 +1,32 @@
 import sys
 sys.setrecursionlimit(10**6)
 
+#떡볶이떡 만들기 - 실전 풀이 방식
+
+N, M = map(int, input().split())
+arrl = list(map(int, input().split()))
+
+x = 0
+newarrl = [0 for i in range(N)]
+R = 0
+
+while True:
+    for i in range(N):
+        if arrl[i] > x:
+            newarrl[i] = arrl[i] - x
+        else:
+            newarrl[i] = 0
+    R = sum(newarrl)
+
+    if M < R:
+        x += 1
+        continue;
+
+    elif M >= R:
+        break;
+
+print(x)
+
 
 #부품 찾기 - 반복문
 
