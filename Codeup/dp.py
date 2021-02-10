40f1a39d574d7f06fff7295a694a0251edaa0f90
sunnyw <vnvnchlrh@gmail.com>
2021-02-10 15:49:25 +0900

    0210 15:49

---
+++ Codeup/dp.py
@@ -0,0 +1,36 @@
+#2602
+
+arr = []
+for i in range(3):
+    arr.append(list(map(int, input().split())))
+
+newarr = []
+for j in range(3):
+    newarr.append([arr[0][j], arr[1][j], arr[2][j]])
+
+for k in range(3):
+    newarr[k] = sum(newarr[k])
+
+arr.append(newarr)
+
+for l in range(4):
+    arr[l].append(sum(arr[l]))
+
+for i in range(4):
+    for j in range(4):
+        print(arr[i][j], end = ' ')
+    print()
+
+#2601 
+
+N = int(input())
+
+arr = [0] * 101
+arr[0] = 1
+arr[1] = 1
+
+for i in range(2, N):
+    arr[i] = arr[i-1] + arr[i-2]
+
+print(arr[N-1])
+
