78ac19ab9a53f9a2769c49d77e19333556da8d70
sunnyw <vnvnchlrh@gmail.com>
2021-02-16 15:12:33 +0900

    210216 15:12

--- PythonThisisCS.Na2020/Chapter9_ShortestPath_FloydWarshall.py
+++ PythonThisisCS.Na2020/Chapter9_ShortestPath_FloydWarshall.py
@@ -20,7 +20,7 @@ for k in range(1, n+1):
     for a in range(1, n+1):
         for b in range(1, n+1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-        
+
 for a in range(1, n+1):
     for b in range(1, n+1):
         if graph[a][b] == INF:
