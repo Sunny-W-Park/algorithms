cc58f0e42741a982029dafb3bda8d8b84868cda8
sunnyw <vnvnchlrh@gmail.com>
2021-11-26 17:58:09 +0900

    211126

--- Codeup/basics#6001-.py
+++ Codeup/basics#6001-.py
@@ -10,6 +10,31 @@ print("Hello World")
 
 print("Hello\nWorld")
 
+#6004
+
+print("\'Hello\'")
+
+#6005
+
+print("\"Hello\"")
+
+#6006
+
+print("\"!@#$%^&*()\'")
+
+#6007
+
+print("\"C:\Download\\'hello'.py\"")
+
+#6008
+
+print("print(\"Hello\\nWorld\")")
+
+#6009
+
+a = input(str())
+print(a)
+
 #6010
 
 #a = map(int, input())
