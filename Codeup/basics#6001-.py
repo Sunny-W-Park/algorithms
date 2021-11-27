e86dc98c3cf485f3a719a6dff7607c5a9603d3d1
sunnyw <vnvnchlrh@gmail.com>
2021-11-27 22:49:17 +0900

    211127 Codeup

--- Codeup/basics#6001-.py
+++ Codeup/basics#6001-.py
@@ -37,11 +37,66 @@ print(a)
 
 #6010
 
-#a = map(int, input())
-#print(a)
-
 a = input()
 print(int(a))
+
+#6011
+
+a = input()
+print(float(a))
+
+#6012
+
+a = input()
+b = input()
+print(a)
+print(b)
+
+#6013
+
+a = input()
+b = input()
+print(str(b))
+print(str(a))
+
+#6014
+
+a = input()
+print(float(a))
+print(float(a))
+print(float(a))
+
+#6015
+
+a, b = map(int, input().split(" "))
+print(a)
+print(b)
+
+#6016
+
+a, b = map(str, input().split(" "))
+print(b + " " + a)
+
+#6017
+
+a = input()
+print(a, a, a)
+
+#6018
+
+a, b = map(int, input().split(":"))
+print(a, b, sep = ":")
+
+#6019
+
+a, b, c = map(int, input().split("."))
+print(c, b, a, sep = "-")
+
+#6020
+
+a, b = map(str, input().split("-"))
+print(a, b, sep = "")
+
 
 #6087
 
