b8176d4a7004c6838b58814ce30ff27b659b6862
sunnyw <vnvnchlrh@gmail.com>
2021-11-28 22:21:30 +0900

    211128

--- Codeup/basics#6001-.py
+++ Codeup/basics#6001-.py
@@ -97,6 +97,79 @@ print(c, b, a, sep = "-")
 a, b = map(str, input().split("-"))
 print(a, b, sep = "")
 
+#6021
+
+a = input()
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+#6022
+
+a = input()
+print(a[0]+""+a[1]+" "+a[2]+""+a[3]+" "+a[4]+""+a[5])
+
+#슬라이싱 사용 시
+
+a = input()
+print(a[0:2]+" "+a[2:4]+" "+a[4:6])
+
+#6023
+
+a = input()
+if a[1] == ":":
+    if a[3] == ":":
+        print(a[4])
+    else:
+        print(a[2:4])
+elif a[2] == ":":
+    if a[4] == ":":
+        print(a[5])
+    else:
+        print(a[3:5])
+
+#6024
+
+a, b = map(str, input().split(" "))
+print(a+b)
+
+#6025
+
+a, b = map(int, input().split(" "))
+print(a+b)
+
+#6026
+
+a = input()
+a = float(a)
+b = input()
+b = float(b)
+print(a+b)
+
+#6027
+
+a = input()
+a = int(a)
+print('%x'% a)
+
+#6028
+
+a = input()
+a = int(a)
+print('%X'% a)
+
+#6029
+
+a = input()
+a = int(a, 16)
+print('%o'% a)
+
+#6030
+
+a = ord(input())
+print(a)
 
 #6087
 
