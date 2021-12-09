32a9746d3bfbbc54e46791b9e3858a127f8a1b4b
sunnyw <vnvnchlrh@gmail.com>
2021-12-10 00:53:37 +0900

    211209

--- BOJ/level1.py
+++ BOJ/level1.py
@@ -1,3 +1,71 @@
 #Level 1 
 
+#2557
+
+print("Hello World!")
+
+#10718
+
+for i in range(2):
+    print("강한친구 대한육군")
+
+#10171
+
+print("\
+\    /\\\n\
+ )  ( ')\n\
+(  /  )\n\
+ \(__)|\
+ ")
+
+#10172
+
+print("\
+|\_/|\n\
+|q p|   /}\n\
+( 0 )\"\"\"\\\n\
+|\"^\"`    |\n\
+||_/=\\\\__|\n\
+")
+
+#1000
+
+a, b = map(int, input().split())
+print(a+b)
+
+#1001
+
+a, b = map(int, input().split())
+print(a-b)
+
+#10998
+
+a, b = map(int, input().split())
+print(a * b)
+
+#1008
+
+a, b = map(int, input().split())
+print(a / b)
+
+#10869
+
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+
+#2588
+
+a = int(input())
+b = int(input())
+r1 = a * (b // 100)
+r2 = a * ((b - (r1 / a * 100)) // 10)
+r3 = a * ((b - (r1 / a * 100) - (r2 / a * 10)))
+print(int(r3))
+print(int(r2))
+print(int(r1))
+print(a * b)
 
--- notepad.py
+++ notepad.py
@@ -1,3 +1,88 @@
+#-------BOJ-------
+
+#2588
+
+a = int(input())
+b = int(input())
+r1 = a * (b // 100)
+r2 = a * ((b - (r1 / a * 100)) // 10)
+r3 = a * ((b - (r1 / a * 100) - (r2 / a * 10)))
+print(int(r3))
+print(int(r2))
+print(int(r1))
+print(a * b)
+
+#10430
+
+A, B, C = map(int, input().split())
+r1 = (A+B)%C
+r2 = ((A%C) + (B%C))%C
+r3 = (A*B)%C
+r4 = ((A%C) * (B%C))%C
+print(r1)
+print(r2)
+print(r3)
+print(r4)
+
+#10869
+
+a, b = map(int, input().split())
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
+
+#1008
+
+a, b = map(int, input().split())
+print(a / b)
+
+#10998
+
+a, b = map(int, input().split())
+print(a * b)
+
+#1001
+
+a, b = map(int, input().split())
+print(a-b)
+
+#1000
+
+a, b = map(int, input().split())
+print(a+b)
+
+#10172
+
+print("\
+|\_/|\n\
+|q p|   /}\n\
+( 0 )\"\"\"\\\n\
+|\"^\"`    |\n\
+||_/=\\\\__|\n\
+")
+
+#10171
+
+print("\
+\    /\\\n\
+ )  ( ')\n\
+(  /  )\n\
+ \(__)|\
+ ")
+
+#10718
+
+for i in range(2):
+    print("강한친구 대한육군")
+
+#2557
+
+print("Hello World!")
+
+#-------Code Up-------
+
 #6098
 
 array = []
