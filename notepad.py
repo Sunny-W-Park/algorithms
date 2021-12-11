305cbd1a20512dc2c12bb38bf04abe26522047f3
sunnyw <vnvnchlrh@gmail.com>
2021-12-11 15:05:53 +0900

    211211

--- notepad.py
+++ notepad.py
@@ -1,5 +1,39 @@
 #-------BOJ-------
 
+#2753
+
+year = int(input())
+
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print("1")
+else:
+    print("0")
+
+#9498
+
+score = int(input())
+
+if 100 >= score >= 90:
+    print("A")
+elif 89 >= score >= 80:
+    print("B")
+elif 79 >= score >= 70:
+    print("C")
+elif 69 >= score >= 60:
+    print("D")
+else:
+    print("F")
+
+#1330
+
+a, b = map(int, input().split())
+if a > b:
+    print(">")
+elif a < b:
+    print("<")
+elif a == b:
+    print("==")
+
 #2588
 
 a = int(input())
