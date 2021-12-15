1878c32886dca7bc2ecb715f81258fa116fb56ce
sunnyw <vnvnchlrh@gmail.com>
2021-12-16 01:16:58 +0900

    211215

--- BOJ/level5.py
+++ BOJ/level5.py
@@ -16,3 +16,64 @@ for j in range(n):
 
 print(min, max)
 
+#2562
+
+list = []
+
+for i in range(9):
+    list.append(int(sys.stdin.readline()))
+
+max = list[0]
+index = 0
+
+for j in range(9):
+    if list[j] > max:
+        max = list[j]
+        index = j+1
+    elif list[j] == max:
+        index = j+1
+
+print(max)
+print(index)
+
+#2577
+
+list = []
+
+for i in range(3):
+    list.append(int(sys.stdin.readline()))
+
+result = [str(list[0] * list[1] * list[2])]
+
+count = [0 for x in range(10)]
+
+for m in range(len(result[0])):
+    for n in range(10):
+        if result[0][m] == str(n):
+            count[n] += 1
+
+for k in range(10):
+    print(count[k])
+
+
+#3052
+
+list = []
+
+for i in range(10):
+    list.append(int(sys.stdin.readline()) % 42)
+
+result = [1 for _ in range(10)]
+
+for j in range(10):
+    for k in range(10):
+        if j != k:
+            if list[j] == list[k]:
+                result[j] = 0
+
+        elif j == k:
+            if list[j] == list[k]:
+                result[j] = 1
+
+print(sum(result))
+
--- notepad.py
+++ notepad.py
@@ -2,6 +2,68 @@ import sys
 
 #-------BOJ-------
 
+#3052
+
+list = []
+
+for i in range(10):
+    list.append(int(sys.stdin.readline()) % 42)
+
+result = [1 for _ in range(10)]
+
+for j in range(10):
+    for k in range(10):
+        if j != k:
+            if list[j] == list[k]:
+                result[j] = 0
+
+        elif j == k:
+            if list[j] == list[k]:
+                result[j] = 1
+
+print(result)
+print(sum(result))
+
+#2577
+
+list = []
+
+for i in range(3):
+    list.append(int(sys.stdin.readline()))
+
+result = [str(list[0] * list[1] * list[2])]
+
+count = [0 for x in range(10)]
+
+for m in range(len(result[0])):
+    for n in range(10):
+        if result[0][m] == str(n):
+            count[n] += 1
+
+for k in range(10):
+    print(count[k])
+
+#2562
+
+list = []
+
+for i in range(9):
+    list.append(int(sys.stdin.readline()))
+
+max = list[0]
+index = 0
+
+for j in range(9):
+    if list[j] > max:
+        max = list[j]
+        index = j+1
+    elif list[j] == max:
+        index = j+1
+
+print(max)
+print(index)
+
+
 #10810
 
 n = int(input())
