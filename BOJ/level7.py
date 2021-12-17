0d5ac7c223b1369f451ff885ee284b2ea89d41fd
sunnyw <vnvnchlrh@gmail.com>
2021-12-18 04:21:30 +0900

    211217

---
+++ BOJ/level7.py
@@ -0,0 +1,16 @@
+#11654
+
+n = input()
+print(ord(n))
+
+#11720
+
+p = input()
+q = input()
+list = []
+
+for i in range(len(str(q))):
+    list.append(int(q[i]))
+
+print(sum(list))
+
--- notepad.py
+++ notepad.py
@@ -2,7 +2,23 @@ import sys
 
 #-------BOJ-------
 
-#1065 **재풀이 요망**
+#11720
+
+p = input()
+q = input()
+list = []
+
+for i in range(len(str(q))):
+    list.append(int(q[i]))
+
+print(sum(list))
+
+#11654
+
+n = input()
+print(ord(n))
+
+#1065 
 
 n = int(input())
 list = []
