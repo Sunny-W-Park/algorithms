70f376f05cd99acc3f9e43048748471fa33aeed1
sunnyw <vnvnchlrh@gmail.com>
2021-12-23 02:50:01 +0900

    211222

--- BOJ/level8.py
+++ BOJ/level8.py
@@ -66,3 +66,47 @@ x = (v - b) / (a - b)
 x_ceil = math.ceil(x)
 print(x_ceil)
 
+#10250
+
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+floor = 0
+room = 0
+result = []
+
+for i in range(n):
+    if arr[i][2] % arr[i][0] == 0:
+        floor = arr[i][0]
+
+        if arr[i][2] // arr[i][0] >= 1:
+            room = arr[i][2] // arr[i][0]
+
+        elif arr[i][2] // arr[i][0] == 0:
+            room = 1
+
+    else:
+        if arr[i][2] // arr[i][0] >= 1:
+            floor = arr[i][2] % arr[i][0]
+            room = arr[i][2] // arr[i][0] + 1
+
+        elif arr[i][2] // arr[i][0] == 0:
+            floor = arr[i][2] % arr[i][0]
+            room = 1
+
+    if room >= 10:
+        result.append(str(floor) + str(room))
+
+    else:
+        result.append(str(floor) + '0' + str(room))
+
+for j in range(n):
+    print(int(result[j]))
+
+#10757
+
+a, b = map(int, input().split())
+print(a+b)
+
