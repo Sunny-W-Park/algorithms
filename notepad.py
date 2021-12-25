ff4039577e7e9d98f8ff7b9bfbe0584614822700
sunnyw <vnvnchlrh@gmail.com>
2021-12-25 23:42:41 +0900

    211225

--- notepad.py
+++ notepad.py
@@ -2,6 +2,25 @@ import sys
 
 #-------BOJ-------
 
+t = int(input())
+
+for i in range(t):
+    x, y = map(int, input().split())
+    distance = y - x
+    result = 1
+    n = round(distance ** (1/2))
+
+    if distance == 1:
+        result = result
+
+    elif n ** 2 < distance:
+        result += 2 * n - 1
+
+    elif n ** 2 >= distance:
+        result += 2 * n - 2
+
+    print(result)
+
 #2839
 
 n = int(input())
