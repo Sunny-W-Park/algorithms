0c5591d7f9d6feed299263d3b8b3644c56bff3e8
sunnyw <vnvnchlrh@gmail.com>
2021-12-27 23:50:21 +0900

    211227

--- notepad.py
+++ notepad.py
@@ -2,6 +2,23 @@ import sys
 
 #-------BOJ-------
 
+#1929
+
+def isPrime(n):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+m, n = map(int, input().split())
+
+for j in range(m, n+1):
+    if isPrime(j):
+        print(j)
+
 #11653
 
 n = int(input())
