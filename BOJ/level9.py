9dc25ba3819280c5a3d689d69eb55b660a41bf72
sunnyw <vnvnchlrh@gmail.com>
2021-12-29 00:38:38 +0900

    211228

--- BOJ/level9.py
+++ BOJ/level9.py
@@ -77,5 +77,65 @@ for j in range(m, n+1):
     if isPrime(j):
         print(j)
 
+#4948
 
+def isPrime(n):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+list = list(range(2, 246912))
+prime = []
+
+for i in list:
+    if isPrime(i):
+        prime.append(i)
+
+k = 1
+while k != 0:
+    k = int(input())
+    count = 0
+    if k == 0:
+        break
+    else:
+        for i in prime:
+            if k < i <= 2*k:
+                count += 1
+    print(count)
+
+
+#9020
+
+def isPrime(n):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+numbers = list(range(2, 10001))
+prime = []
+for i in numbers:
+    if isPrime(i):
+        prime.append(i)
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    a = 0
+    b = 0
+    for j in range(len(prime)):
+        if prime[j] >= n/2:
+            if n - prime[j] in prime:
+                b = prime[j]
+                a = n - prime[j]
+                break
+    print(a, b)
 
--- review.py
+++ review.py
@@ -18,3 +18,36 @@ result_num = sorted(natural_num - generated_num)
 for k in result_num:
     print(k)
 
+#2021.12.28 
+
+#9020
+
+def isPrime(n):
+    if n == 1:
+        return False
+    else:
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+numbers = list(range(2, 10001))
+prime = []
+for i in numbers:
+    if isPrime(i):
+        prime.append(i)
+
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    a = 0
+    b = 0
+    for j in range(len(prime)):
+        if prime[j] >= n/2:
+            if n - prime[j] in prime:
+                b = prime[j]
+                a = n - prime[j]
+                break
+    print(a, b)
+
