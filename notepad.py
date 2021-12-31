3d30d88af77343c4fc9c91d7fbbaa12db40e7c41
sunnyw <vnvnchlrh@gmail.com>
2021-12-31 11:54:39 +0900

    211231

--- notepad.py
+++ notepad.py
@@ -2,8 +2,32 @@ import sys, math
 
 #-------BOJ-------
 
-#1002
+#1002 #재풀이 요망
 
+t = int(input())
+
+for i in range(t):
+    x1, y1, r1, x2, y2, r2 = map(float, input().split())
+    d = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
+
+    if d == 0:
+        if r1 == r2:
+            print(-1)
+        else:
+            print(0)
+
+    elif r1 == d or r2 == d:
+        print(1)
+
+    else:
+        if r1 + r2 == d:
+            print(1)
+
+        elif r1 + r2 > d:
+            print(2)
+
+        else:
+            print(0)
 
 
 #3053
