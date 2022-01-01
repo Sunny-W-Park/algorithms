bb3442a64289257710110587441c7bea95ab3f32
sunnyw <vnvnchlrh@gmail.com>
2022-01-01 23:52:55 +0900

    220101

--- notepad.py
+++ notepad.py
@@ -10,25 +10,17 @@ for i in range(t):
     x1, y1, r1, x2, y2, r2 = map(float, input().split())
     d = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
 
-    if d == 0:
-        if r1 == r2:
-            print(-1)
-        else:
-            print(0)
+    if d == 0 and r1 == r2:
+        print(-1)
 
-    elif r1 == d or r2 == d:
+    elif r1 + r2 == d or abs(r1 - r2) == d:
         print(1)
 
+    elif abs(r1 - r2) < d < r1 + r2:
+        print(2)
+
     else:
-        if r1 + r2 == d:
-            print(1)
-
-        elif r1 + r2 > d:
-            print(2)
-
-        else:
-            print(0)
-
+        print(0)
 
 #3053
 
