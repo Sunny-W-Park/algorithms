b5efd9f9fa37c6401195a7e576f9781b558bfea5
sunnyw <vnvnchlrh@gmail.com>
2022-01-05 21:06:56 +0900

    220105

--- BOJ/level12.py
+++ BOJ/level12.py
@@ -1,4 +1,18 @@
-#2750
+#10989
+
+n = int(input())
+
+check_list = [0 for _ in range(10001)]
+
+for i in range(n):
+    num = int(sys.stdin.readline())
+    check_list[num] = check_list[num] + 1
+
+for i in range(10001):
+    if check_list[i] != 0:
+        for j in range(check_list[i]):
+            print(i)        #해당 index(i)를 횟수(j) 만큼 출력
+
 #2750
 
 n = int(input())
