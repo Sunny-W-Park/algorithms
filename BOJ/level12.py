823cc7b2b52511a4bb3d15cbffd3e99690d0ce7a
sunnyw <vnvnchlrh@gmail.com>
2022-01-06 21:29:27 +0900

    210106

--- BOJ/level12.py
+++ BOJ/level12.py
@@ -1,3 +1,57 @@
+import sys
+
+#1427
+
+N = str(input())
+list = []
+
+for i in range(len(N)):
+    list.append(int(N[i]))
+
+list_sorted = sorted(list, reverse = True)
+
+result = ''
+
+for i in range(len(list_sorted)):
+    result += str(list_sorted[i])
+
+print(result)
+
+#2108
+
+n = int(sys.stdin.readline())
+
+list = []
+many_list = []
+
+for i in range(n):
+    list.append(int(sys.stdin.readline()))
+
+list_sorted = sorted(list)
+count = [0 for _ in range(8002)]
+
+for i in list_sorted:
+    count[i+4000] += 1
+
+many_value = max(count)
+
+for i in range(8002):
+    if count[i] == many_value:
+        many_list.append(i-4000)
+
+#산술평균
+print('{:.0f}'.format(sum(list_sorted)/n))
+#중앙값
+print(list_sorted[round((n-1)/2)])
+#최빈값
+if len(many_list) == 1:
+    print(many_list[0])
+else:
+    print(many_list[1])
+#범위
+print(list_sorted[n-1] - list_sorted[0])
+
+
 #10989
 
 n = int(input())
