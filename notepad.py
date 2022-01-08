3a8b36b5acd53561c251256e589a6224fc0202e7
sunnyw <vnvnchlrh@gmail.com>
2022-01-08 20:18:20 +0900

    220108

--- notepad.py
+++ notepad.py
@@ -2,6 +2,32 @@ import sys, math
 
 #-------BOJ--------
 
+#2231
+
+input = sys.stdin.readline
+N = int(input())
+result = []
+
+a = 0
+
+def function(a):
+    num = a
+    for i in range(len(str(a))):
+        num += int(str(a)[i])
+    return num
+
+while a <= N:
+    a += 1
+    if function(a) == N:
+        result.append(a)
+        break
+
+if len(result) == 0:
+    print(0)
+else:
+    print(result[0])
+
+
 #2798
 
 input = sys.stdin.readline
