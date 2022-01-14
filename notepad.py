34ca32832b11bd87af59c94f4b5b7b2b6eac9ad9
sunnyw <vnvnchlrh@gmail.com>
2022-01-14 20:48:01 +0900

    220114

--- notepad.py
+++ notepad.py
@@ -2,6 +2,35 @@ import sys, math
 
 #-------BOJ--------
 
+#1037
+
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+arr = sorted(arr)
+
+if len(arr) == 1:
+    print(arr[0] * arr[0])
+
+else:
+    print(arr[0] * arr[-1])
+
+#5086
+
+a, b = 1, 1
+while a!= 0 and b!= 0:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    if b % a == 0:
+        print('factor')
+    elif a % b == 0:
+        print('multiple')
+    else:
+        print('neither')
+
+
 #1931 2차 풀이
 
 input = sys.stdin.readline
