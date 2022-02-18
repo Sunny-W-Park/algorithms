a154420aa3830cc7148567a4b0d70882452dc543
sunnyw <vnvnchlrh@gmail.com>
2022-02-19 08:59:09 +0900

    220219

--- baekjoon/level2.py
+++ baekjoon/level2.py
@@ -84,3 +84,26 @@ while A >= 24:
 
 print(A, B)
 
+#2480
+
+import sys
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+
+if a == b and b == c:
+    print(10000 + a * 1000)
+
+elif a == b and b != c:
+    print(1000 + a * 100)
+
+elif b == c and a != b:
+    print(1000 + b * 100)
+
+elif a == c and a != b:
+    print(1000 + a * 100)
+
+elif a != b and b != c:
+    print(max(a, b, c) * 100)
+
+
---
+++ programmers/SQL-kit-03-GROUPBY.sql
@@ -0,0 +1,10 @@
+--SET1: 고양이와 개는 몇 마리 있을까
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_ID) AS 'count'
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE;
+
+--SET2: 동명 동물 수 찾기
+
+
