c2d919119c191be0369d148214551ae1055846ab
sunnyw <vnvnchlrh@gmail.com>
2022-02-20 23:36:18 +0900

    220220

--- review.py
+++ review.py
@@ -1,3 +1,27 @@
+#2022.02.20
+
+#11403 해설
+
+n = int(input())
+visit = [0 for i in range(n)]
+s = []
+def dfs(v):
+    for i in range(n):
+        if visit[i] == 0 and s[v][i] == 1:
+            visit[i] = 1
+            dfs(i)
+for i in range(n):
+    s.append(list(map(int, input().split())))
+for i in range(n):
+    dfs(i)
+    for j in range(n):
+        if visit[j] == 1:
+            print(1, end=' ')
+        else:
+            print(0, end=' ')
+    print()
+    visit = [0 for i in range(n)]
+
 #2022.02.18
 
 #2178 BFS로 풀기
