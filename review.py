204b158cdd8af5b34e518cc52eba5a3c88b2710c
sunnyw <vnvnchlrh@gmail.com>
2022-02-21 12:46:52 +0900

    220221

--- review.py
+++ review.py
@@ -1,5 +1,30 @@
 #2022.02.20
 
+#11403 재풀이
+
+N = int(input())
+g = []
+for _ in range(N):
+    g.append(list(map(int, input().split())))
+
+def dfs(i):
+    for j in range(N):
+        if visited[j] == 0 and g[i][j] == 1:
+            visited[j] = 1
+            dfs(j)
+
+visited = [0 for _ in range(N)]
+for i in range(N):
+    dfs(i)
+    for j in range(N):
+        if visited[j] == 1:
+            print(1, end = " ")
+        if visited[j] == 0:
+            print(0, end = " ")
+    print()
+    visited = [0 for _ in range(N)]
+
+
 #11403 해설
 
 n = int(input())
