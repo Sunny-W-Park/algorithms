1a1ebe2013cbb730320fd8dd0065abbbfeec5146
sunnyw <vnvnchlrh@gmail.com>
2022-02-26 20:29:58 +0900

    220226

---
+++ baekjoon/1916.py
@@ -0,0 +1,33 @@
+#1916
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+INF = int(1e9)
+distance = [INF for _ in range(N+1)]
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist > distance[now]:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+start, end = map(int, input().split())
+dijkstra(start)
+print(distance[end])
+
