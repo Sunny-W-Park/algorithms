66a31b7812f809c0982a6d13afa8957482dd1b08
sunnyw <vnvnchlrh@gmail.com>
2022-03-02 11:29:53 +0900

    220302

--- baekjoon/level24-dfs-bfs.py
+++ baekjoon/level24-dfs-bfs.py
@@ -217,4 +217,57 @@ N, K = map(int, input().split())
 count = [0] * 100001
 bfs()
 
+#7576
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+maps = []
+for _ in range(N):
+    maps.append(list(map(int, input().split())))
+
+count_zero = 0
+pos_tomato = []
+
+for i in range(N):
+    for j in range(M):
+        if maps[i][j] == 0:
+            count_zero += 1
+        if maps[i][j] == 1:
+            pos_tomato.append((i, j))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+count_days = 0
+
+if count_zero == 0:
+    print(0)
+
+else:
+    queue = deque(pos_tomato)
+    while len(queue) != 0:
+        for k in range(len(queue)):
+            x, y = queue.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0 <= nx <= N-1 and 0 <= ny <= M-1:
+                    if maps[nx][ny] == 0:
+                        maps[nx][ny] = 1
+                        queue.append((nx, ny))
+        count_days += 1
+
+    impos = False
+    for i in range(N):
+        for j in range(M):
+            if maps[i][j] == 0:
+                impos = True
+
+    if impos == True:
+        print(-1)
+    else:
+        print(count_days-1)
+
 
