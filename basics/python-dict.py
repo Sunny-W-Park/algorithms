4dd1f68f84c835569959a2eb600b9705729afe72
sunnyw <vnvnchlrh@gmail.com>
2022-03-10 00:54:34 +0900

    220310

---
+++ basics/python-dict.py
@@ -0,0 +1,19 @@
+#반올림, 올림, 내림
+
+import math
+
+a = 3.14
+print(round(a))
+print(math.ceil(a))
+print(math.floor(a))
+
+#조합
+
+#import itertools
+#
+#arr = [1, 2, 3, 4]
+#comb = itertools.combinations(arr, 2)
+#print(list(comb))
+#comb_list = list(comb)
+#print(len(comb_list))
+
