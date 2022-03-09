a5cd86f72be7bcd32635c2a2e810627f63a2e33c
sunnyw <vnvnchlrh@gmail.com>
2022-03-10 00:42:20 +0900

    220310

--- baekjoon/level12-sorting.py
+++ baekjoon/level12-sorting.py
@@ -43,37 +43,31 @@ for i in range(10001):
 
 #2108
 
-n = int(sys.stdin.readline())
+import sys
+input = sys.stdin.readline
+N = int(input())
+arr = []
+dp = [0 for _ in range(8001)]
+for _ in range(N):
+    x = int(input())
+    arr.append(x)
+    dp[x+4000] += 1
 
-list = []
-many_list = []
-
-for i in range(n):
-    list.append(int(sys.stdin.readline()))
-
-list_sorted = sorted(list)
-count = [0 for _ in range(8002)]
-
-for i in list_sorted:
-    count[i+4000] += 1
-
-many_value = max(count)
-
-for i in range(8002):
-    if count[i] == many_value:
-        many_list.append(i-4000)
-
-#산술평균
-print('{:.0f}'.format(sum(list_sorted)/n))
-#중앙값
-print(list_sorted[round((n-1)/2)])
-#최빈값
-if len(many_list) == 1:
-    print(many_list[0])
+avg = sum(arr)/N
+if avg < 0:
+    print(-1 * round(-1 * avg))
 else:
-    print(many_list[1])
-#범위
-print(list_sorted[n-1] - list_sorted[0])
+    print(round(avg))
+print(sorted(arr)[N//2])
+mf = []
+for i in range(len(dp)):
+    if dp[i] == max(dp):
+        mf.append(i-4000)
+if len(mf) > 1:
+    print(sorted(mf)[1])
+else:
+    print(sorted(mf)[0])
+print(max(arr) - min(arr))
 
 #1427
 
