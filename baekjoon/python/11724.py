dbb618e0488d453202c0eca2d36a6fb6dcde7348
Sunny-W-Park <vnvnchlrh@korea.ac.kr>
2022-04-05 23:46:02 +0900

    220405

---
+++ baekjoon/python/11724.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0 for _ in range(N)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(start, visited):
+    global connected
+    visited[start-1] = 1
+    for i in graph[start]:
+        if visited[i-1] == 0:
+            dfs(i, visited)
+            connected = True
+
+count = 0
+for i in range(1, N+1):
+    connected = False
+    if visited[i-1] == 0:
+        dfs(i, visited)
+        if connected == True:
+            count += 1
+        else:
+            count += 1
+
+print(count)
+    
